fe4f7270b04fb0ef9db0d64a1d24191137349e82
pranavbhatthero <pbhatt@salesforce.com>
2024-03-01 01:51:19 -0500

    Remove and update comments

--- force-app/main/default/lwc/accountExecutiveSummaryCustomAPEX/accountExecutiveSummaryCustomAPEX.js
+++ force-app/main/default/lwc/accountExecutiveSummaryCustomAPEX/accountExecutiveSummaryCustomAPEX.js
@@ -11,53 +11,46 @@ export default class AccountExecutiveSummaryCustomAPEX extends LightningElement 
     isSummaryGenerated = false;
     isPreview = true;
 
+    // "Generate Account Executive Summary" Button Click
     handleClick() {
         this.isLoading = true;
-        console.log("handleClick() is working !");
-        //wire service to get the Account details
-        console.log("this.recordId " + this.recordId);
+        // wire service to get the Account details
         generate({ accountId: this.recordId })
             .then((result) => {
                 this.isLoading = false;
                 this.aeSummaryBody = result;
-                console.log("this.aeSummaryBody is :");
-                console.log(this.aeSummaryBody);
-                console.log("generate() working !");
                 this.isSummaryGenerated = true;
             })
             .catch((error) => {
                 this.error = error;
-                console.log("not working !")
                 this.isLoading = false;
             });
     }
 
+    // "Edit Summary" On/Off toggle
     handleToggleChange(event) {
         if (event.target.checked) {
             this.isPreview = false;
-            console.log("Toggle change : False : " + this.isPreview);
         } else {
             this.isPreview = true;
-            console.log("Toggle change : True : " + this.isPreview);
         }
     }
 
+    // Track changes made while Editing the Generated Summary
     handleSummaryChange(event){
-        // handleChange in Slack / Email / Field
         this.aeSummaryBody = event.target.value;
-        console.log("this changed value " + this.aeSummaryBody);
     }
-    
-    handlePublish(){
-        // handleChange in Slack / Email / Field
-        console.log("publish this value in Slack / Email / Field " + this.aeSummaryBody);
 
+    // Can be published anywhere, publish to Salesforce in this use case
+    handlePublish(){
+
+        // create fields to be populated with data
         const fields = {};
         fields['Id'] = this.recordId; //populate it with current record Id
         fields['AI_Generated_AE_Summary_using_APEX__c'] = this.aeSummaryBody; //populate any fields which you want to update like this
         
+        // feed recordInput, used to update Salesforce Record
         const recordInput = { fields };
-        console.log(" constant record Input : " + recordInput);
         updateRecord(recordInput) 
             .then(() => {
                     this.dispatchEvent(
@@ -66,8 +59,7 @@ export default class AccountExecutiveSummaryCustomAPEX extends LightningElement 
                                     message: 'Published Executive Summary to the Accoun team',
                                     variant: 'success',
                             }),
-                            // Display fresh data in the form
-                            // return refreshApex(this.account);
+                            // Can display fresh data back if needed refreshApex(this.account);
                     );
             }) 
             .catch(error => {
@@ -82,7 +74,6 @@ export default class AccountExecutiveSummaryCustomAPEX extends LightningElement 
             });
         this.aeSummaryBody = '';
         this.isSummaryGenerated = false;
-        console.log("PUBLISHING !!!");
     }
 
 }
